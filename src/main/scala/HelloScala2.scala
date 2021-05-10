e56d029e251351e956a60e35750ebe93e1ccd603
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-10 20:11:29 +0300

    Sparse Arrays Exercise

--- src/main/scala/HelloScala2.scala
+++ src/main/scala/HelloScala2.scala
@@ -1,3 +1,8 @@
 object HelloScala2 extends App {
   println("Welcome all to our Spring 2021 Scala course")
+  val myNumbers = (1 to 100).toArray //if we had truly big range we might not want to convert to Array
+  //range would be more efficient for memory and also a bit for speed
+  val myBigNumbers = myNumbers.map(n => n*200)
+  myBigNumbers.slice(0,10).foreach(println) //remember last index in slice is not included
+
 }
---
+++ src/main/scala/SparseArrays.scala
@@ -0,0 +1,7 @@
+object SparseArrays extends App {
+  // https://www.hackerrank.com/challenges/sparse-arrays/problem
+  //TODO read and parse ../resources/arr1.txt
+  //TODO read and parse ../resources/arr2.txt
+  //return (println) the results
+
+}
