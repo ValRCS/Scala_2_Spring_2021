6b8aae3539a04520e7129de7285be7c6fce22eea
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-10 19:45:27 +0300

    Hello Scala 2 commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "Scala_2_Spring_2021"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ src/main/scala/HelloScala2.scala
@@ -0,0 +1,3 @@
+object HelloScala2 extends App {
+  println("Welcome all to our Spring 2021 Scala course")
+}
