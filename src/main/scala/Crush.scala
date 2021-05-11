dc315230a3f0379dcfd271a7eb5681993b3226ef
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-11 20:14:09 +0300

    Crush arrays challenge

---
+++ src/main/scala/Crush.scala
@@ -0,0 +1,11 @@
+object Crush extends App {
+  //TODO Solve the following problem
+  //https://www.hackerrank.com/challenges/crush/problem
+  //TODO read and parse ../resources/crush1.txt
+  //TODO read and parse ../resources/crush2.txt
+  //you need to perform 3 operations
+  //and find the maximum in the new array
+
+  //think how well this would work on large values of operations like
+  //1000 1000
+}
