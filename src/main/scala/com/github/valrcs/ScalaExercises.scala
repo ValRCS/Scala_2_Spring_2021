19e5d0bfe2bcbb1079a07fdd61c09a879291d4f2
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-15 10:54:30 +0300

    Filter exercise for list

--- src/main/scala/com/github/valrcs/ScalaExercises.scala
+++ src/main/scala/com/github/valrcs/ScalaExercises.scala
@@ -18,8 +18,48 @@ object ScalaExercises extends App {
   newAnyMap.foreach(println)
 
   //prefer specific types but it is possible to create sets of Any types
-  val mySet = Set("Michigan", "Ohio", 12)
+  val mySet = scala.collection.mutable.Set("Michigan", "Ohio", 12)
   println(mySet.size)
+  mySet += ("Valdis") //can add single new item
+  mySet ++= Seq("Apples", "Oranges") //can add multiples
+  println(mySet)
+  mySet -= "Ohio" //can remove singles from a set
+  mySet --= Seq("Michigan", 12, 9000) //so no errors on removing nonexistent members of the set
+  println(mySet)
 
+  val mySet1 = Set("Michigan", "Ohio", "Wisconsin", "Iowa")
+  val mySet2 = Set("Wisconsin", "Michigan", "Minnesota")
+  val aNewSet = mySet1 intersect mySet2 // NOTE: You can use the "&" operator
+  println(aNewSet)
+  val aNewSet2 = mySet1 union mySet2 // NOTE: You can also use the "|" operator
+  println(aNewSet2)
+  val mySet3 = Set("Wisconsin", "Michigan")
+  println(mySet2 subsetOf mySet1)
+  println(mySet2.subsetOf(mySet1)) //full syntax, same as above line
+  println(mySet3 subsetOf mySet1)
 
+  val aNewDiffSet = mySet1 diff mySet2 // Note: you can use the "&~" operator if you *really* want to.
+  //this is non symmetrical difference
+  println(aNewDiffSet.equals(Set("Ohio", "Iowa")) )
+
+  //tuples
+  //https://www.scala-exercises.org/std_lib/tuples
+  val student = ("Sean Rogers", 21, 3.5)
+  val (name, age, gpa) = student //this is so called tuple unpacking
+  println(student)
+  val tuple2Swap = ("Apple", 3).swap //only for Tuple2 swap is available
+  println(tuple2Swap)
+
+  //https://www.scala-exercises.org/std_lib/lists
+  val a = List(1, 2, 3)
+  val b = List(1, 2, 3)
+  println(a eq b) //tests if they are same object
+  println(a == b) //tests if they have same contents
+
+  val aList = List(1, 3, 5, 7, 9)
+  val bList = aList.filterNot(v => v == 5)
+  println(bList)
+  //TODO write a filter which is same as filterNot above
+  val cList = aList.filter(n => true)
+  println(bList == cList) //should be true
 }
