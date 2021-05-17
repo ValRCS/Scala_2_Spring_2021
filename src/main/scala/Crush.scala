d9e40497a15053a9159f4058b92150ae2fc2b7f1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-17 19:15:21 +0300

    Crush hand coded example

--- src/main/scala/Crush.scala
+++ src/main/scala/Crush.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ArrayBuffer
+
 object Crush extends App {
   //TODO Solve the following problem
   //https://www.hackerrank.com/challenges/crush/problem
@@ -10,4 +12,27 @@ object Crush extends App {
 
   //think how well this would work on large values of operations like
   //1000 1000
+  val seq = (1 to 5).map(_ => 0)
+  val myArray = ArrayBuffer.fill(5)(0)
+  println(seq)
+//  seq(3) = 555
+//  myArray(3) = 777
+  //1 2 100
+  myArray(1-1) += 100
+  myArray(2-1) += 100
+  //2nd instruction 2 5 100
+  myArray(2-1) += 100
+  myArray(3-1) += 100
+  myArray(4-1) += 100
+  myArray(5-1) += 100
+  //3rd instruction for crush
+  myArray(3-1) += 100
+  myArray(4-1) += 100
+
+  println(myArray)
+  println(myArray.max)
+  //TODO do this brute force solution by loops
+
+  //TODO think if you really need that many loops ...
+
 }
