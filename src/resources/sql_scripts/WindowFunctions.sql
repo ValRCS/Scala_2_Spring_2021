223212ad5ef373245c8ac82d69695fa244e6df95
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-20 20:05:19 +0300

    FIRST LAST Values

--- src/resources/sql_scripts/WindowFunctions.sql
+++ src/resources/sql_scripts/WindowFunctions.sql
@@ -104,12 +104,116 @@
 --WHERE
 --    AlbumId = 1;
 
-SELECT AlbumId, Name, printf('%,d',Bytes) Size,
-FIRST_VALUE(Name) OVER (
-	PARTITION BY AlbumId
-	ORDER BY Bytes DESC) BiggieTrack,
-LAST_VALUE(Name) OVER (
-	PARTITION BY AlbumId
-	ORDER BY Bytes DESC) MiniTrack
-FROM tracks t
-ORDER BY Name DESC;
+--SELECT a2.Title AlbumName, t.AlbumId, Name, printf('%,d',Bytes) Size,
+--FIRST_VALUE(Name) OVER (
+--	PARTITION BY t.AlbumId
+--	ORDER BY Bytes DESC) BiggieTrack,
+--LAST_VALUE(Name) OVER (
+--	PARTITION BY t.AlbumId
+--	ORDER BY Bytes DESC) MiniTrack
+--FROM tracks t
+--JOIN albums a2
+--ON a2.AlbumId = t.AlbumId
+--ORDER BY AlbumName DESC;
+
+--CREATE VIEW v_CustomerInvoices
+--AS
+--SELECT
+--	CustomerId,
+--	strftime('%Y',InvoiceDate) Year,
+--	SUM( total ) Total
+--FROM
+--	invoices
+--	GROUP BY CustomerId, strftime('%Y',InvoiceDate);
+
+--CREATE VIEW v_InvoicesLead
+--AS
+--SELECT CustomerId, Year, Total,
+--	LEAD(Total,1,0 ) OVER (
+--	PARTITION BY CustomerId
+--	ORDER BY Year) NextYearTotal
+--FROM v_CustomerInvoices;
+--WHERE CustomerId = 1;
+--ORDER BY
+--	CustomerId,
+--	Year,
+--	Total;
+
+--SELECT *, NextYearTotal - Total YearToYearDelta FROM v_InvoicesLead vil ;
+
+--SELECT
+--	CustomerId,
+--	Year,
+--	Total,
+--	LAG ( Total, 1, 0 ) OVER (
+--		ORDER BY Year
+--	) PreviousYearTotal
+--FROM
+--	v_CustomerInvoices vci
+--WHERE
+--	CustomerId = 4;
+
+--SELECT
+--	CustomerId,
+--	Year,
+--	Total,
+--	LAG ( Total,1,0) OVER (
+--		PARTITION BY CustomerId
+--		ORDER BY Year ) PreviousYearTotal,
+--		Total - PreviousYearTotal Delta
+--FROM
+--	v_CustomerInvoices;
+
+--SELECT
+--    AlbumId, Name,
+--    Milliseconds Length,
+--    NTH_VALUE(name,2) OVER (
+--        ORDER BY Milliseconds DESC
+----        RANGE BETWEEN
+----            UNBOUNDED PRECEDING AND
+----            UNBOUNDED FOLLOWING
+--    ) SecondLongestTrack
+--FROM
+--    tracks;
+
+--SELECT
+--    a2.Name Artist, a.Title AlbumName, t.AlbumId,
+--    t.Name,
+--    Milliseconds Length,
+--    NTH_VALUE ( t.Name,2 ) OVER (
+--        PARTITION BY t.AlbumId
+--        ORDER BY Milliseconds DESC
+--        RANGE BETWEEN
+--            UNBOUNDED PRECEDING AND
+--            UNBOUNDED FOLLOWING
+--    ) AS SecondLongestTrack
+--FROM
+--    tracks t
+--JOIN albums a
+--ON t.AlbumId = a.AlbumId
+--JOIN artists a2
+--ON a.ArtistId = a2.ArtistId ;
+--
+--SELECT *,
+--SUM(Total) OVER (
+--	PARTITION BY CustomerId
+--	ORDER BY Year
+--) CumulativeTotal,
+--COUNT(Total)  OVER (
+--PARTITION BY CustomerId
+--ORDER BY Year
+--) YearsOfSales,
+--AVG(Total)  OVER (
+--PARTITION BY CustomerId
+--ORDER BY Year
+--) AverageSales
+--FROM v_CustomerInvoices vci
+
+--TODO Create View of Tracks lets call it v_RankedTracks
+--WIth extra columns:
+-- giving average length of song (among songs in the album)
+-- song ranking number among the songs in the album from the shortest
+-- first song of the album
+-- number of songs in the album
+-- and for extra kicks album real name and also artist
+
