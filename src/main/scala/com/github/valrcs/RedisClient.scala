94d8bd80e6ac47ee2a6475a9c5b7872d97fe72d8
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-25 20:27:18 +0300

    Redis connection example

--- src/main/scala/com/github/valrcs/RedisClient.scala
+++ src/main/scala/com/github/valrcs/RedisClient.scala
@@ -6,8 +6,15 @@ object RedisClient extends App {
   //we just a need a database first :)
   val port = 10283
   val db = "FunStoreNov20"
-  val secret = Some("use real pw here")
+  val secret = Some("need to add it outside") //FIXME DO not show open secrets, do not commit ...
 
 
   val r = new RedisClient("redis-10283.c62.us-east-1-4.ec2.cloud.redislabs.com", port, 0, secret)
+
+  r.set("name", "Valdis")
+  r.incr(key = "visits")
+
+  println(r.get("name"))
+  val visits = r.get(key="visits")
+  println(s"Number of visits: $visits")
 }
