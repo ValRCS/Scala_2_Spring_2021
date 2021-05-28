2b87568966a3e14ea4f86fca0f595bf14057b767
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-28 17:47:26 +0300

    Connecting to MongoDB setup working with syn env password

--- src/main/scala/com/github/valrcs/MongoDBConnection.scala
+++ src/main/scala/com/github/valrcs/MongoDBConnection.scala
@@ -10,29 +10,39 @@ import java.lang.Thread.sleep
 object MongoDBConnection extends App {
   println("Testing MongoDB database")
 
-  val user = "badscalaVal"
-  val pw = "needreal" //FIXME do not submit real pw to GIT!!!!
+  val user = "reader"
+  val pw = Some(scala.util.Properties.envOrElse("MONGOPW", "nosuchsecret")).getOrElse("nothing") //FIXME do not submit real pw to GIT!!!!
   val dbname = "sample_restaurants"
   val collectionName = "restaurants"
 
   //uri - universal resource identificator
   //make sure when connectiong that cluster0......net is correct for your databas
-  val uri: String = s"mongodb+srv://$user:$pw@cluster0.whr5v.mongodb.net"
+  val uri: String = s"mongodb+srv://$user:$pw@cluster0.whr5v.mongodb.net/sample_restaurants?retryWrites=true&w=majority"
   System.setProperty("org.mongodb.async.type", "netty")
   val client : MongoClient  = MongoClient(uri)
   val db: MongoDatabase = client.getDatabase(dbname)
   val collection = db.getCollection(collectionName)
 
   //https://mongodb.github.io/mongo-scala-driver/1.0/reference/observables/
-  collection.find(equal("name", "456 Cookies Shop")).subscribe((doc: Document) => println(doc.toJson()),
+//  collection.find(equal("name", "456 Cookies Shop")).subscribe((doc: Document) => println(doc.toJson()),
+//    (e: Throwable) => println(s"There was an error: $e"),
+//    () => println("Completed!"))
+//  collection.find(equal("borough", "Brooklyn")).subscribe((doc: Document) => println(doc.toJson()),
+//    (e: Throwable) => println(s"There was an error: $e"),
+//    () => println("Completed!"))
+  collection.find(
+    and(
+    equal("borough", "Brooklyn"),
+    equal("cuisine","Polish")))
+    .subscribe((doc: Document) => println(doc.toJson()),
     (e: Throwable) => println(s"There was an error: $e"),
     () => println("Completed!"))
 
 
   println(collection.find().first().toString)
 
-  val brooklyn_chinese = collection.find(and(equal("borough", "Brooklyn"),
-    equal("cuisine", "Chinese")))//so this example of
+//  val brooklyn_chinese = collection.find(and(equal("borough", "Brooklyn"),
+//    equal("cuisine", "Chinese")))//so this example of
 
 
 //  val chinese = collection.find(and(equal("cuisine", "Chinese"),equal("borough","Brooklyn")))//so this example of
