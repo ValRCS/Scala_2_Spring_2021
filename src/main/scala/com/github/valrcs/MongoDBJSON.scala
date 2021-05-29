a9358b762ce5b05bbea78311a363d439cb77b5f6
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-05-29 15:45:04 +0300

    Update and Delete examples

--- src/main/scala/com/github/valrcs/MongoDBJSON.scala
+++ src/main/scala/com/github/valrcs/MongoDBJSON.scala
@@ -2,6 +2,9 @@ package com.github.valrcs
 
 import org.bson.BSONEncoder
 import org.mongodb.scala.{Document, MongoClient, MongoDatabase}
+import org.mongodb.scala.model.Filters._
+
+import org.mongodb.scala.model.Updates._
 
 import java.lang.Thread.sleep
 
@@ -36,12 +39,57 @@ object MongoDBJSON extends App {
 //          (e: Throwable) => println(s"There was an error when inserting data: $e"),
 //          () => println("Completed insertion or not!"))
   //tail because we already inserted first element above
-  val docs = jsonStringsTrim.tail.map(txtToDoc) //same as .map(it => txtToDoc(it))
+//  val docs = jsonStringsTrim.tail.map(txtToDoc) //same as .map(it => txtToDoc(it))
+  //if we want to insert everything we prepare docs of every item
+  val docs = jsonStringsTrim.map(txtToDoc) //same as .map(it => txtToDoc(it))
 
-      collection.insertMany(docs).subscribe(res => println(res),
-        (e: Throwable) => println(s"There was an error when inserting tons of data: $e"),
-        () => println("Completed insertion!"))
+//  collection.insertMany(docs).subscribe(res => println(res),
+//        (e: Throwable) => println(s"There was an error when inserting tons of data: $e"),
+//        () => println("Completed insertion!"))
 
-  sleep(1000)
+
+
+  //so when deleting we need to pass a filter what to delete
+  //if we give a blank Document this will be like DELETE * FROM table_name; in SQL
+//  collection.deleteMany(Document()).subscribe(res => println(res),
+//            (e: Throwable) => println(s"There was an error when DELETING tons of data: $e"),
+//            () => println("Completed DELETION!"))
+
+
+  //UPDATE
+//  collection.updateOne(equal("borough", "Brooklyn"),
+//    combine(set("borough", "Queens"), currentDate("lastModified"))
+//  ).subscribe(res => println(res),
+//                (e: Throwable) => println(s"There was an error when UPDATING single  of data: $e"),
+//                () => println("Completed UPDATE!"))
+
+  //in SQLite
+  //UPDATE from_json_texts
+  //SET cuisine = 'Italian',
+  //    column_2 = new_value_2
+  //WHERE
+  //    name LIKE '%Restaurant%'
+//  collection.updateMany(regex("name","Restaurant"),
+//    combine(
+//      set("cuisine", "Italian"),
+//      currentDate("lastModified")
+//    ))
+//    .subscribe(res => println(res),
+//                    (e: Throwable) => println(s"There was an error when UPDATING tons of data: $e"),
+//                    () => println("Completed MASS UPDATE!"))
+
+  collection.updateMany(notEqual("grades.0.grade","A"),
+    combine(
+      set("critique", "AVOID!"), //new field (key) automatically created,
+      inc("grades.0.score", -100), //atomic operation works or does not
+      currentDate("lastModified")
+    ))
+    .subscribe(res => println(res),
+      (e: Throwable) => println(s"There was an error when UPDATING tons of data: $e"),
+      () => println("Completed MASS UPDATE!"))
+
+
+
+  sleep(3000) //we need to give MongoDB a bit of time to process a remote transaction
   client.close()
 }
