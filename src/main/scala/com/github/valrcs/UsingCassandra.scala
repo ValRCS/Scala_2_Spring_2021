1845d632a328600f201e076c68bbff0017032118
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-06-03 21:53:37 +0300

    set Results to table, read results

--- src/main/scala/com/github/valrcs/UsingCassandra.scala
+++ src/main/scala/com/github/valrcs/UsingCassandra.scala
@@ -1,6 +1,6 @@
 package com.github.valrcs
 
-import com.github.valrcs.CassandraExample.cassandraExample
+import com.github.valrcs.CassandraExample.{cassandraExample, getResults, setResults}
 
 object UsingCassandra extends App {
   println("Testing Cassandra connection")
@@ -8,11 +8,22 @@ object UsingCassandra extends App {
   val host = "cassandra-3fec232b-mihtal-a81e.aivencloud.com" //your url will be slightly different
   val port = 13249 //port might also be a different
   val username = "avnadmin" //most likely the same
-  val password = "yourown" //FIXME get from enviroment, DO NOT commit real credentials to git!!
+  val password = "useyourown" //FIXME get from enviroment, DO NOT commit real credentials to git!!
   //  val caPath = "C:\\certs\\"
   val caPath = "./src/resources/certs/ca.pem" //you need to download your own cert
 
   cassandraExample(host=host,port=port,username=username,password=password, caPath=caPath)
   println("Lets hold our fingers crossed")
+  setResults(host=host,
+    port=port,
+    username=username,
+    password=password,
+    caPath=caPath,
+    keyspace = "example_keyspace",
+    id = 9000,
+    message = "Hello from Space on Jun 3rd")
 
+  val query = "SELECT id, message FROM example_java"
+  val results = getResults(host=host,port=port,username=username,password=password, caPath=caPath, keyspace = "example_keyspace", query = query)
+  results.foreach(println)
 }
